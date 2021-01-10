25e0c75f092c45c3f660d40189a61547e7fe8a9b
johnie <bjw2264@qq.com>
2021-01-10 20:30:42 +0800

    init forestry

---
+++ forestry/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <span v-html="settings" />
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      settings: "Copyright &copy; 2019",
+    }
+  },
+}
+</script>
+
+<style scoped>
+.footer {
+  font-size: 0.8rem;
+  padding: 6rem 0;
+}
+</style>
---
+++ forestry/src/pages/Index.vue
@@ -0,0 +1,58 @@
+<template>
+  <Layout>
+    <div class="container">
+      <Hero />
+      <ProjectsGrid :projects="projects" />
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  list: allStrapiProject {
+    edges {
+      node {
+        id
+        title
+        categories {
+          name
+        }
+        cover {
+          url
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import Hero from "@/components/Hero"
+import ProjectsGrid from "@/components/ProjectsGrid"
+export default {
+  components: {
+    Hero,
+    ProjectsGrid,
+  },
+  metaInfo: {
+    title: "Portfolio",
+  },
+  computed: {
+    // graphql 怎么更好的定义数据结构？
+    projects() {
+      const _arr = this.$page.list.edges.map((item) => {
+        return {
+          id: item.node.id,
+          title: item.node.title,
+          cover: "http://182.92.3.2:1337" + item.node.cover.url,
+          categories: item.node.categories.map((v) => v.name),
+        }
+      })
+      return _arr.reverse()
+    },
+  },
+}
+</script>
+
+<style>
+</style>
